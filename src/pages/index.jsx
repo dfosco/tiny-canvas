712229772fb3c40e57de656381c855b045847a77
Daniel Fosco <dfosco@github.com>
2025-01-26 12:23:16 +0100

    v0.5 for whiteboard -- wip

---
+++ src/pages/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Canvas from '../canvas.jsx';
+
+function App() {
+  
+  return (    
+      <Canvas>
+          
+      </Canvas>
+  );
+}
+
+export default App;
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import generouted from '@generouted/react-router/plugin'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: { port: 1234 }
+export default defineConfig({ 
+  plugins: [react(), generouted()],
+  server: { port: 1234 },
 })
