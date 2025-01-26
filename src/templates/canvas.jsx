24610c32aa3ad2a260b752048166febf77b3ee60
Daniel Fosco <dfosco@github.com>
2025-01-26 20:42:47 +0100

    Basic styling on draggables

--- src/templates/canvas.jsx
+++ src/templates/canvas.jsx
@@ -1,11 +1,17 @@
-import { useRef } from 'react';
+import { useRef, Children, cloneElement } from 'react';
 import { useDraggable } from '@neodrag/react';
+import "../css/canvas.css";
 
-function Canvas(props) {
+function DraggableWrapper({ children }) {
   const draggableRef = useRef(null);
   const { isDragging } = useDraggable(draggableRef, {
     axis: 'both',
-    grid: [10, 10],
+    // grid: [10, 10],
+    bounds: 'body',
+    threshold: { delay: 30, distance: 4 },
+    defaultClass: 'drag',
+    defaultClassDragging: 'on',
+    defaultPosition: { x: 0, y: 0 },
     onDragStart: (data) => {
       console.log('Started dragging:', data);
     },
@@ -14,13 +20,30 @@ function Canvas(props) {
     }
   });
 
+  // const rotation = Math.random() * 4 - 2;;
+  const rotationVariations = useRef(Math.random() * 4 - 2).current;
+  const rotation = isDragging ? `${rotationVariations}deg` : '0deg';
+  
   return (
-    <main>
-      <div className="canvas">
-        <article ref={draggableRef} style={{ cursor: isDragging ? 'grabbing' : 'grab' }}>
-          {props.children}
-        </article>
+    <article 
+      ref={draggableRef} 
+      style={{  cursor: isDragging ? 'grabbing' : 'grab' }}
+    >
+      <div className="draggable" style={{ transform: isDragging && `rotate(${rotation})`, transition: 'transform ease-in-out 150ms' }}>
+        {children}
       </div>
+    </article>
+  );
+}
+
+function Canvas({ children }) {
+  return (
+    <main className="canvas">
+          {Children.map(children, (child, index) => (
+            <DraggableWrapper key={index}>
+              {child}
+            </DraggableWrapper>
+          ))}
     </main>
   );
 }
