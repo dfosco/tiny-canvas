01d448d8f796d6ed7416632493e009d390d1d520
Daniel Fosco <dfosco@github.com>
2025-01-27 19:58:02 +0100

    More routing stuffs [2]

--- src/app.jsx
+++ src/app.jsx
@@ -3,7 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './css/reset.css'
 import './css/globals.css'
 import { Routes } from '@generouted/react-router'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import { ThemeProvider, BaseStyles } from '@primer/react'
+
+const Routes = () => <RouterProvider router={createBrowserRouter(routes, { basename: '/storyboard' })} />
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
