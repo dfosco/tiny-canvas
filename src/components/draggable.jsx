f6465fb85f8a8321c55811cb25c1b96f4480cc3f
Daniel Fosco <dfosco@github.com>
2025-01-27 16:35:26 +0100

    More visual improvements

--- src/components/draggable.jsx
+++ src/components/draggable.jsx
@@ -4,20 +4,21 @@ import "./draggable.css";
 import "../css/globals.css";
 import { findDragId, refreshStorage, getQueue, saveDrag } from '../templates/utils';
 
-
 // Pull value from --golden-number in globals.css, trim `px` from the end, and save it as a int in a const
 const golden = parseInt(getComputedStyle(document.documentElement).getPropertyValue('--golden-number')) / 2;
 
 function Draggable({ children }) {
+
+  // Set up references for the draggable element and pull data from localStorage
   const draggableRef = useRef(null);
   const dragId = useRef(findDragId(children)).current;
   const queue = useRef(getQueue(dragId)).current;
 
   // Add new effect for on-translation class
   const translation_ms = 250;
-
   const [onTranslation, setOnTranslation] = useState(false);
 
+  // On page load, apply class .on-translation to all draggables and remove it after 150ms (coming from a variable)
   useEffect(() => {
     document.documentElement.style.setProperty('--translation_ms', `${translation_ms}ms`);
     const draggableEl = draggableRef.current;
@@ -34,8 +35,8 @@ function Draggable({ children }) {
     }
   }, []);
   
+  // Apply saved coordinates from localStorage to the draggable elements on page load
   useEffect(() => {
-    // On page load, apply class .on-translation to all draggables and remove it after 150ms (coming from a variable)
     refreshStorage();
     if (draggableRef.current && queue) {
       setPosition({ x: queue.x, y: queue.y }); 
@@ -46,27 +47,37 @@ function Draggable({ children }) {
 
   const [position, setPosition] = useState({ x: 0, y: 0 });
   
+  // Get the main element and check for data-hasGrid attribute
+  const mainElement = document.querySelector('main');
+  const checkGrid = mainElement?.getAttribute('data-hasGrid') === 'true';
+  const hasGrid = checkGrid ? [golden, golden] : undefined;
+
   const { isDragging } = useDraggable(draggableRef, {
     axis: 'both',
-    grid: [golden, golden],
+    grid: hasGrid, // Use grid only if data-hasGrid is present
     bounds: 'body',
     threshold: { delay: 30, distance: 4 },
     defaultClass: 'drag',
     defaultClassDragging: 'on',
     defaultClassDragged: 'off',
-    // applyUserSelectHack: true,
-    position: {  x: position.x, y: position.y },
-    // onDragStart: () => setPosition({ x: 0, y: 0 }),
+    applyUserSelectHack: true,
+    position: { x: position.x, y: position.y },
     onDrag: ({ offsetX, offsetY }) => setPosition({ x: offsetX, y: offsetY }),
     onDragEnd: (data) => {
       setPosition({ x: data.offsetX, y: data.offsetY }); 
-      saveDrag(dragId, data.offsetX, data.offsetY); 
+      saveDrag(dragId, data.offsetX, data.offsetY);
       console.log('DragEnd to:', position);
     }
   });
 
-  const rotationVariations = useRef(Math.random() * 4 - 2).current;
+  // Rotate draggable element randomly between -2 and 2 degrees on each drag
+  const [rotationVariations, setRotationVariations] = useState(Math.random() < 0.5 ? -2 : 2);
   const rotation = (isDragging || onTranslation) ? `${rotationVariations}deg` : '0deg';
+
+  useEffect(() => {
+    const newRotation = Math.random() < 0.5 ? -2 : 2;
+    setRotationVariations(newRotation);
+  }, [isDragging]);
   document.documentElement.style.setProperty('--rotation', `${rotation}`);
   
   return (
--- src/templates/canvas.jsx
+++ src/templates/canvas.jsx
@@ -2,12 +2,12 @@ import { Children } from 'react';
 import Draggable from '../components/draggable';
 import styles from "./canvas.module.css";
 
-function Canvas({ children, centered = true }) {
+function Canvas({ children, centered = true, grid = false }) {
 
   const centeredMode = centered ? {display: 'flex', flexDirection: 'column', gap: '1rem', alignItems: 'center', justifyContent: 'center'} : null;
 
   return (
-    <main className={styles.canvas} style={centeredMode}>
+    <main className={styles.canvas} style={centeredMode} data-hasGrid={grid}>
           {Children.map(children, (child, index) => (
             <Draggable key={index}>
               {child}
